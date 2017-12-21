163772b1f2ef0bfd42d1a5e845eecb4d945631ea
Mohammed Sabihuddin <sabihuddinzeeshan@gmail.com>
2017-12-21 10:39:25 +0530

    Update SnapShot Retention period script.py

--- SnapShot Retention period script.py
+++ SnapShot Retention period script.py
@@ -47,27 +47,25 @@ L = len(snapid['Snapshots'])
 i = 0
 los = []
 while ( i < L ):
-  a=snapid['Snapshots'][i]['SnapshotId']
-  los.append(a)
-  i = i+1
+	a=snapid['Snapshots'][i]['SnapshotId']
+  	los.append(a)
+  	i = i+1
 for list in los:
-n = re.Snapshot(list)
-	    times = str(n.start_time)
-	    year = int(times[0:4])
-	    month = int(times[5:7])
-	    day = int(times[8:10])
-	    ST = date( year , month , day )
-	    localtime = time.localtime(time.time())
-	    cyear = int( localtime.tm_year)
-	    cmonth = int(localtime.tm_mon)
-	    cday = int(localtime.tm_mday)
-	    cdate = (cyear,cmonth,cday)
-	    cdate = date(cyear,cmonth,cday)
-	    diff = cdate - ST
-	    DiffDays = diff.days
-	    if ( DiffDays >= 1 ): Replace the number with your retention no of days  eg for 2 days [if ( DiffDays >= 2  )]
-		    out = n.delete()
-		    print (out)
-
-
-
+	n = re.Snapshot(list)
+	times = str(n.start_time)
+	year = int(times[0:4])
+	month = int(times[5:7])
+	day = int(times[8:10])
+	ST = date( year , month , day )
+	localtime = time.localtime(time.time())
+	cyear = int( localtime.tm_year)
+	cmonth = int(localtime.tm_mon)
+	cday = int(localtime.tm_mday)
+	cdate = date(cyear,cmonth,cday)
+	diff = cdate - ST
+	DiffDays = diff.days
+	if ( DiffDays >= 1 ):
+					#Replace the number with your retention no of days  eg for 2 days [if ( DiffDays >= 2  )]
+		out = n.delete()
+		
+""" ******************** END OF SCRIPT *********************************************** """ 
