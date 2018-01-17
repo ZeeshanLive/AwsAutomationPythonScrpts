7bedcc5f9431ff1451f2d46f5720b11f779d644c
Mohammed Sabihuddin <sabihuddinzeeshan@gmail.com>
2018-01-17 10:07:31 +0530

    Update CrossRegionAMIBackup.py

--- CrossRegionAMIBackup.py
+++ CrossRegionAMIBackup.py
@@ -4,4 +4,4 @@ ImageAmi = 'Your AMI ID' # AMI ID OF Your Image
 ImageRegion = ' Region in which your image resides' # IN WHICH REGION YOUR AMI RESIDES
 bck_region = 'your region name in which you want to create backup' #REGION IN WHICH YOU WANT TO COPY YOUR IMAGE
 bck = boto3.client('ec2',region_name = bck_region )
-CopyImage = bck.copy_image(Description="'Image copy from my'+'-'+ImageRegion",Name='crossregionbackupimage',SourceImageId = ImageAmi , SourceRegion = ImageRegion)
+CopyImage = bck.copy_image(Description="CrossRegionImageBackup",Name='crossregionbackupimage',SourceImageId = ImageAmi , SourceRegion = ImageRegion)
