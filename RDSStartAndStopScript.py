c6e6046514376735bc92a510888f166d28f69d27
Mohammed Sabihuddin <sabihuddinzeeshan@gmail.com>
2018-03-16 15:35:29 +0530

    Update RDSStartAndStopScript.py

--- RDSStartAndStopScript.py
+++ RDSStartAndStopScript.py
@@ -1,1 +1,13 @@
 
+import boto3
+rdsobj = boto3.client('rds')
+DBId = ['mydbinstance'] #replace with the name of your DB instance Identifier, 
+i=0
+while i < len(DBId):
+	DbObj = rdsobj.describe_db_instances(DBInstanceIdentifier=DBId[i])
+	StatusOfDb = DbObj['DBInstances'][i]['DBInstanceStatus']
+	if StatusOfDb == 'available':
+		se = client.stop_db_instance(DBInstanceIdentifier=DBId[i])
+	else:
+		se = client.start_db_instance(DBInstanceIdentifier=DBId[i])
+	i = i+1
