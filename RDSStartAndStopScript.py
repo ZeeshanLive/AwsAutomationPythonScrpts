474a5e25f2a0d50d8d058cbbe8c29b3f34b2971e
Mohammed Sabihuddin <sabihuddinzeeshan@gmail.com>
2018-03-19 14:49:44 +0530

    Update RDSStartAndStopScript.py

--- RDSStartAndStopScript.py
+++ RDSStartAndStopScript.py
@@ -2,12 +2,11 @@
 import boto3
 rdsobj = boto3.client('rds')
 DBId = ['mydbinstance'] #replace with the name of your DB instance Identifier, 
-i=0
-while i < len(DBId):
-	DbObj = rdsobj.describe_db_instances(DBInstanceIdentifier=DBId[i])
-	StatusOfDb = DbObj['DBInstances'][i]['DBInstanceStatus']
+for a in DBId:
+	DbObj = rdsobj.describe_db_instances(DBInstanceIdentifier=a)
+	StatusOfDb = DbObj['DBInstances'][0]['DBInstanceStatus']
 	if StatusOfDb == 'available':
-		se = rdsobj.stop_db_instance(DBInstanceIdentifier=DBId[i])
+		se = rdsobj.stop_db_instance(DBInstanceIdentifier=a)
 	else:
-		se = rdsobj.start_db_instance(DBInstanceIdentifier=DBId[i])
+		se = rdsobj.start_db_instance(DBInstanceIdentifier=a)
 	i = i+1
